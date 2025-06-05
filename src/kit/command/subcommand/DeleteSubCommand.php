34c69ee6f3846156f7ee6ef536bfe7cd4e71b56b
jesusmxc5663 <jeus56245@gmail.com>
2025-06-04 21:12:34 -0500

    Update DeleteSubCommand.php

--- src/kit/command/subcommand/DeleteSubCommand.php
+++ src/kit/command/subcommand/DeleteSubCommand.php
@@ -1,25 +1,25 @@
 <?php
 
-namespace duel\command\subcommand;
+namespace kit\command\subcommand;
 
 use pocketmine\command\CommandSender;
 
-use duel\command\DuelCommand;
-use duel\command\SubCommand;
+use kit\command\DuelCommand;
+use kit\command\SubCommand;
 
 class DeleteSubCommand extends SubCommand
 {
 	
-	const PREFIX = '§r§b[Duel]';
+	const PREFIX = '§r§b[PVP]';
 	
 	/** @var DuelCommand */
 	private $command;
 	
 	/**
 	  * CreateSubCommand constructor.
-	  * @param DuelCommand $command
+	  * @param KitCommand $command
 	  */
-	public function __construct(DuelCommand $command)
+	public function __construct(KitCommand $command)
 	{
 		$this->command = $command;
 	}
@@ -31,17 +31,17 @@ class DeleteSubCommand extends SubCommand
 	public function execute(CommandSender $sender, array $args)
 	{
 		if (count($args) < 2) {
-			 $sender->sendMessage(self::PREFIX.' §r§b/duel deletar <nome> §r§fpara deletar uma partida.');
+			 $sender->sendMessage(self::PREFIX.' §r§b/kitpvp deletar <nome> §r§fpara borrar una partida');
 			 return false;
 		}
 		$manager = $this->command->loader->arenaManager;
 		
 		if (!$manager->existsArena($args[1])) {
-			 $sender->sendMessage(self::PREFIX.' §r§cA partida '.$args[1].' não existe!');
+			 $sender->sendMessage(self::PREFIX.' §r§cA partida '.$args[1].' no existe!');
 			 return false;
 		}
 		$manager->removeArena($args[1]);
-		$sender->sendMessage(self::PREFIX.' §r§aPartida '.$args[1].' deletada.');
+		$sender->sendMessage(self::PREFIX.' §r§aPartida '.$args[1].' borrada.');
 		return true;
 	}
 }
